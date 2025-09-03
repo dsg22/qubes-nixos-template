0c0596833467900172297551f0db2fbe6480bd94
Davíð Steinn Geirsson <david@dsg.is>
2025-09-03 23:28:43 +0000

    Make example config more useful (not aggressively minimal)
    Minimal is good, but most people probably want basic desktop-y things. Still
    keeping things small.

--- examples/configuration.nix
+++ examples/configuration.nix
@@ -12,7 +12,19 @@
 
   hardware.graphics.enable = true;
 
+  # Enable icons, themes, dconf and other stuff useful for a graphical VM.
+  services.xserver.desktopManager.xfce.enable = true;
+
+  # HiDPI config
+  #services.xserver.dpi = 284;
+  #environment.variables= {
+  #  "GDK_SCALE" = "2";
+  #  "QT_SCALE_FACTOR" = "2.0";
+  #  "PLASMA_USE_QT_SCALING" = "1";
+  #};
+
   environment.systemPackages = with pkgs; [
     xterm
+    xfce.xfce4-terminal
   ];
 }
